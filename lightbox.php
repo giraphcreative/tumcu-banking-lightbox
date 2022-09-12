e49e41f7c7900c582ced7015ef68605af44f37a2
James Pederson <james@jpederson.com>
2022-09-12 17:19:07 -0500

    Bump css/js.

--- lightbox.php
+++ lightbox.php
@@ -29,8 +29,8 @@ add_action( 'wp_footer', 'banking_lightbox' );
 
 // include the main.js script in the header on the front-end.
 function merger_assets() {
-	wp_enqueue_style( 'merger-css', plugin_dir_url( __FILE__ ) . 'lightbox.css?v=3' );
-	wp_enqueue_script( 'merger-js', plugin_dir_url( __FILE__ ) . 'lightbox.js?v=3', array( 'jquery' ), false, true );
+	wp_enqueue_style( 'merger-css', plugin_dir_url( __FILE__ ) . 'lightbox.css?v=4' );
+	wp_enqueue_script( 'merger-js', plugin_dir_url( __FILE__ ) . 'lightbox.js?v=4', array( 'jquery' ), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'merger_assets' );
 
